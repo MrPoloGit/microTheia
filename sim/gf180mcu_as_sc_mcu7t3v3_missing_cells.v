f45d3ff63d972a7785e08aabb4c66757ff6f3c51
MrPoloGit <therealmr.polo1@gmail.com>
2026-06-14 12:54:51 -0700

    Work

--- sim/gf180mcu_as_sc_mcu7t3v3_missing_cells.v
+++ sim/gf180mcu_as_sc_mcu7t3v3_missing_cells.v
@@ -1387,33 +1387,6 @@ module gf180mcu_as_sc_mcu7t3v3__fill_1(
 
 endmodule
 
-module gf180mcu_as_sc_mcu7t3v3__fill_2(
-	input VPW,
-	input VNW,
-	input VDD,
-	input VSS
-);
-
-endmodule
-
-module gf180mcu_as_sc_mcu7t3v3__fill_4(
-	input VPW,
-	input VNW,
-	input VDD,
-	input VSS
-);
-
-endmodule
-
-module gf180mcu_as_sc_mcu7t3v3__fill_8(
-	input VPW,
-	input VNW,
-	input VDD,
-	input VSS
-);
-
-endmodule
-
 // dfxtp_4: D flip-flop, positive-edge, drive strength 4.
 // Behaviorally identical to dfxtp_2; size only affects drive strength.
 module gf180mcu_as_sc_mcu7t3v3__dfxtp_4(
